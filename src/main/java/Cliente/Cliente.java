8c4ac48058f2004d413ff822d3a944f58011b4d3
iranhonorato <antonioihsj@al.insper.edu.br>
2025-08-26 17:41:54 -0300

    Inicializando instâncias da classe clientes

--- src/main/java/Cliente/Cliente.java
+++ src/main/java/Cliente/Cliente.java
@@ -1,4 +1,13 @@
 package Cliente;
+import java.time.LocalDate;
+
 
 public class Cliente {
+    private String cpf;
+    private String nome;
+    private LocalDate dataNascimento;
+    private Float salario;
+
+    }
+
 }
