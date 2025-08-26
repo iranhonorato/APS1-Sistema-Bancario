ce797fd86b50a1178133458d73cbbd093640d445
iranhonorato <antonioihsj@al.insper.edu.br>
2025-08-26 18:23:09 -0300

    Implementando metodos get na classe ContaCorrente

--- src/main/java/ContaCorrente/ContaCorrente.java
+++ src/main/java/ContaCorrente/ContaCorrente.java
@@ -6,5 +6,28 @@ public class ContaCorrente {
     private Float saldo;
     private Float limite;
 
+    public ContaCorrente(String agencia, String numero, Float saldo, Float limite) {
+        this.agencia = agencia;
+        this.numero= numero;
+        this.saldo = saldo;
+        this.limite = limite;
+    };
+
+    public String getAgencia() {
+        return this.agencia;
+    };
+
+    public String getNumero () {
+        return this.numero;
+    };
+
+    public Float getSaldo() {
+        return this.saldo;
+    };
+
+    public Float getLimite() {
+        return this.limite;
+    };
+
 
 }
