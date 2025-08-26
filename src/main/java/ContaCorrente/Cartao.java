ecaa0e4053e6e75030404e781e88fd9cd546b886
iranhonorato <antonioihsj@al.insper.edu.br>
2025-08-26 18:35:40 -0300

    Instanciando a classe Cartao

--- src/main/java/ContaCorrente/Cartao.java
+++ src/main/java/ContaCorrente/Cartao.java
@@ -1,4 +1,12 @@
 package ContaCorrente;
 
+import java.time.LocalDate;
+
 public class Cartao {
+    private String numeroCartao;
+    private String tipo;
+    private LocalDate validade;
+    private String status;
+
+
 }
