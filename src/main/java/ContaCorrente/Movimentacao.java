aaaaeb479e303b64ceb730acb2bacca9d030a2d9
iranhonorato <antonioihsj@al.insper.edu.br>
2025-08-27 17:38:34 -0300

    Inicializando classe Movimentacao

--- src/main/java/ContaCorrente/Movimentacao.java
+++ src/main/java/ContaCorrente/Movimentacao.java
@@ -1,4 +1,18 @@
 package ContaCorrente;
+import java.time.LocalDate;
 
 public class Movimentacao {
+    private Float valor;
+    private String tipo;
+    private LocalDate data;
+
+    public Movimentacao(Float valor, String tipo, LocalDate data) {
+        this.valor = valor;
+        this.tipo = tipo;
+        this.data = data;
+    }
+
+
 }
+
+
